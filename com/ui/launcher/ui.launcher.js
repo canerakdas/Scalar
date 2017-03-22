52630f3c32cc866518b3ebd7f5d8b4379af71165
Caner Akdaş <canerakdas@gmail.com>
2017-03-22 07:41:59 +0300

    Fix

--- com/ui/launcher/ui.launcher.js
+++ com/ui/launcher/ui.launcher.js
@@ -8,26 +8,29 @@ scalar.ui.launcher = function () {
 
   setInterval(function () {
     scalar.exec('cd com/bash && bash applicationList.sh', function (output) {
-      this.apps = eval('[' + output + ']');
-      this.current = this.apps[this.apps.length - 1];
+      if (output !== window.appTemp) {
+        window.appTemp = output;
+        this.apps = eval('[' + output + ']');
+        this.current = this.apps[this.apps.length - 1];
 
-      this.current = this.current.active.slice(this.current.active.indexOf("="), this.current.active.length - 1).replace("=", '').trim().replace('"', '');
-      document.querySelector('#launcher').innerHTML = "";
-      for (var i = 0; i < this.apps.length - 1; i++) {
-        if (this.apps[i].title != 'scalar' && this.apps[i].command[0] != 'scalar') {
-          if (this.apps[i].title == this.current) {
-            scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'app-' + i, class: 'app active', 'text': this.apps[i].command[0].slice(0, 1).toUpperCase() }));
-            //this.scalar.ui.append('#app-' + i, scalar.ui.createElement('img', { 'id': 'icon-' + i, 'src': this.iconTheme + this.apps[i].command[0] + '.svg' }));
-            document.querySelector('#app-' + i).setAttribute('onclick', `scalar.exec('wmctrl -R "` + this.apps[i].title + `"',function(){})`);
-          } else {
-            scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'app-' + i, class: 'app', 'text': this.apps[i].command[0].slice(0, 1).toUpperCase() }));
-            //this.scalar.ui.append('#app-' + i, scalar.ui.createElement('img', { 'id': 'icon-' + i, 'src': this.iconTheme + this.apps[i].command[0] + '.svg' }));
-            document.querySelector('#app-' + i).setAttribute('onclick', `scalar.exec('wmctrl -R "` + this.apps[i].title + `"',function(){})`);
+        this.current = this.current.active.slice(this.current.active.indexOf("="), this.current.active.length - 1).replace("=", '').trim().replace('"', '');
+        document.querySelector('#launcher').innerHTML = "";
+        for (var i = 0; i < this.apps.length - 1; i++) {
+          if (this.apps[i].title != 'scalar' && this.apps[i].command[0] != 'scalar') {
+            if (this.apps[i].title == this.current) {
+              scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'app-' + i, class: 'app active', 'text': this.apps[i].command[0].slice(0, 1).toUpperCase() }));
+              //this.scalar.ui.append('#app-' + i, scalar.ui.createElement('img', { 'id': 'icon-' + i, 'src': this.iconTheme + this.apps[i].command[0] + '.svg' }));
+              document.querySelector('#app-' + i).setAttribute('onclick', `scalar.exec('wmctrl -R "` + this.apps[i].title + `"',function(){})`);
+            } else {
+              scalar.ui.append('#launcher', scalar.ui.createElement('div', { 'id': 'app-' + i, class: 'app', 'text': this.apps[i].command[0].slice(0, 1).toUpperCase() }));
+              //this.scalar.ui.append('#app-' + i, scalar.ui.createElement('img', { 'id': 'icon-' + i, 'src': this.iconTheme + this.apps[i].command[0] + '.svg' }));
+              document.querySelector('#app-' + i).setAttribute('onclick', `scalar.exec('wmctrl -R "` + this.apps[i].title + `"',function(){})`);
+            }
           }
         }
       }
     });
-  }, 2000);
+  }, 250);
 };
 
 scalar.ui.launcher.config = {
